e0510058609f94a53054e719b11ab8e7c12f0517
w3scout <info@w3scouts.com>
2013-07-11 08:50:52 +0200

    Added support for Contao 3.0 and 3.1

--- system/modules/form_pdf/FormPDF.php
+++ system/modules/form_pdf/FormPDF.php
@@ -386,8 +386,18 @@ class FormPDF extends Backend
 	protected function sendMail($arrSubmitted,$arrForm=null,$bolIsConfirmationMail=false)
 	{
 		// Include library
-		require_once(TL_ROOT.'/plugins/swiftmailer/swift_required.php');
-		
+        if (version_compare(VERSION, '2.11', '<='))
+        {
+            require_once(TL_ROOT.'/plugins/swiftmailer/swift_required.php');
+        }
+        elseif (version_compare(VERSION, '2.11', '>') && version_compare(VERSION, '3.1', '<'))
+        {
+            require_once(TL_ROOT . '/system/vendor/swiftmailer/swift_required.php');
+        }
+        elseif (version_compare(VERSION, '3.0', '>')) {
+            require_once(TL_ROOT . '/system/modules/core/vendor/swiftmailer/swift_required.php');
+        }
+
 		$arrRecipients = array();
 		$arrSenders = array();
 		$arrAttachments = array();
@@ -695,8 +705,21 @@ class FormPDF extends Backend
 
 		// Include library
 		require_once(TL_ROOT . '/system/config/tcpdf.php');
-		require_once(TL_ROOT . '/plugins/tcpdf/tcpdf.php');
-		require_once(TL_ROOT . '/plugins/tcpdf/htmlcolors.php');
+
+        if (version_compare(VERSION, '2.11', '<='))
+        {
+            require_once(TL_ROOT . '/plugins/tcpdf/tcpdf.php');
+            require_once(TL_ROOT . '/plugins/tcpdf/htmlcolors.php');
+        }
+        elseif (version_compare(VERSION, '2.11', '>') && version_compare(VERSION, '3.1', '<'))
+        {
+            require_once(TL_ROOT . '/system/vendor/tcpdf/tcpdf.php');
+            require_once(TL_ROOT . '/system/vendor/tcpdf/htmlcolors.php');
+        }
+        elseif (version_compare(VERSION, '3.0', '>')) {
+            require_once(TL_ROOT . '/system/modules/core/vendor/tcpdf/tcpdf.php');
+            require_once(TL_ROOT . '/system/modules/core/vendor/tcpdf/include/tcpdf_colors.php');
+        }
 
 		// Create new PDF document
 		$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true);
@@ -728,7 +751,13 @@ class FormPDF extends Backend
 		$pdf->setLanguageArray($l);
 
 		// Initialize document and add a page
-		$pdf->AliasNbPages();
+        if (version_compare(VERSION, '3.1', '<')) {
+		    $pdf->AliasNbPages();
+        }
+        else {
+            #$pdf->AliasNbPages();
+        }
+
 		$pdf->AddPage();
 
 		// Set font
